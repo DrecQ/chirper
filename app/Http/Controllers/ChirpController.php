89a200f4cf480fa10349e810ba89769f727d888b
Your Name <you@example.com>
2024-12-08 07:13:20 +0100

    deuxieme controlleur function pour  view

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response; 
+use Illuminate\View\View;
+
 
 
 
@@ -13,10 +15,9 @@ class ChirpController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response 
+    public function index(): View
     {
-        //
-        return response('Hello, World!');
+        return view('chirps.index');
     }
 
     /**
